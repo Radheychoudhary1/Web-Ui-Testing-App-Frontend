e194b0c2c0f4f6dadd33fe4209a459804de29605
Radheychoudhary1 <radheychoudhary000@gmail.com>
2026-03-12 00:52:44 +0530

    change vite api path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-// 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/Web-Ui-Testing-App-Frontend/",   // IMPORTANT
+  base: "/Web-Ui-Testing-App-Frontend/",   // keep this for GitHub Pages
   plugins: [vue()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
   }
 });
